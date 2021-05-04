f5052c28d4247e953f1ec6872e48a115be7cc5f0
Igor <kennyigrovojkanal@gmail.com>
2021-05-04 23:21:08 +0300

    add

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("04 05 2021 <Текст строки>= Между задницей и диваном доллар никогда не пролетит. - Билл Гейтс");
+    }
+}
